6cf6350c579f45d4720153cfa0080aa55f73e2af
Mazhar <mazghul@gmail.com>
2019-02-07 19:31:43 +0530

    Creating a tabbed activity and addign a graph fragment.

---
+++ app/src/main/java/wilp/bits/iotmanufacturing/Fragment/GraphFragment.java
@@ -0,0 +1,51 @@
+package wilp.bits.iotmanufacturing.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import wilp.bits.iotmanufacturing.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GraphFragment extends Fragment {
+
+
+    public GraphFragment() {
+        // Required empty public constructor
+    }
+
+    public static GraphFragment newInstance() {
+        GraphFragment fragment = new GraphFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_graph, container, false);
+        GraphView graph = root.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+        return root;
+    }
+
+}
